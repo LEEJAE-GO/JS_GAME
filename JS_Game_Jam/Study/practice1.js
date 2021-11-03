e506a3a573efd462fe4ad89284cc188757c644c5
LeeJam <ddo35@naver.com>
2021-11-03 23:08:33 +0900

    211103 study update

--- JS_Game_Jam/Study/practice1.js
+++ JS_Game_Jam/Study/practice1.js
@@ -1,7 +1,21 @@
-function add() {
-  var one = 1;
-  var two = 2;
-  document.write(one + two);
-}
+var answer_number = document.getElementById("inputNumber");
+var numoftime = 0;
+var num = Math.floor(Math.random() * 101);
 
-add()
+function check() {
+  if(answer_number.value > num){
+    numoftime += 1;
+    alert(answer_number.value + "보다 작습니다!");
+    answer_number.value = "";
+  }
+  else if(answer_number.value < num){
+    numoftime += 1;
+    alert(answer_number.value + "보다 큽니다!");
+    answer_number.value = "";
+  }
+  else{
+    alert("정답입니다! 확인한 횟수 : " + numoftime);
+    numoftime = 0;
+    window.location.reload();
+  }
+}
