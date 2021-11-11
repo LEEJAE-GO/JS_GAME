f536c52247560d8e6f18d91db38077606f33d4d8
LeeJam <ddo35@naver.com>
2021-11-11 22:45:57 +0900

    211111 움직이는 물체 그리기

--- JS_Game_Jam/Study_02/game.js
+++ JS_Game_Jam/Study_02/game.js
@@ -5,22 +5,44 @@ const canvas_2d = canvas.getContext("2d");
 let object_time;
 let timer = 0;
 const frame = 60; //컴퓨터 디스플레이 프레임 수에 맞게 변경
+let keys = [];
+
+let upkey = false;
+let downkey = false;
+let rightkey = false;
+let leftkey = false;
+
 
 canvas.width = back.scrollWidth;
 canvas.height = back.scrollHeight;
 
+let x = canvas.width / 2;
+let y = canvas.height / 2;
+let width = 50;
+let height = 50;
+let speed = 1;
+
 canvas_2d.fillStyle = "red";
 
+document.addEventListener("keydown",onKey,false);
+document.addEventListener("keyup",Keyup,false);
 
-const UserImg = new Image();
-UserImg.src = "4gak.png";
-let x = 425;
-let y = 225;
-UserImg.onload = function (){
-  canvas_2d.drawImage(UserImg, x, y,50,50);
+
+
+function drawrect() {
+  canvas_2d.beginPath();
+
+  canvas_2d.rect(x,y,width,height);
+  canvas_2d.strokeStyle = "lightgreen";
+  canvas_2d.fillRect = "lightgreen";
+  canvas_2d.stroke(); 
+  
+  canvas_2d.closePath();
 }
 
 function randnCreate(){
+  
+
   var locate_x = (Math.random()*850).toFixed(2);
   var locate_y = (Math.random()*450).toFixed(2);
 
@@ -40,14 +62,67 @@ function timestemp(){
   }
 } // 게임 플레이 시간 생성
 
+function onKey(key) {
+  keys[key.keyCode] = true;
+
+  if(key.keyCode == 37 ){
+    leftkey = true;
+  }
+  if(key.keyCode == 38){
+    downkey = true;
+  }
+  if(key.keyCode == 39){
+    rightkey = true;
+  }
+  if(key.keyCode == 40){
+    upkey = true;
+  }
+}
+
+function Keyup(key) {
+  keys[key.keyCode] = false;
+
+  if(key.keyCode == 37){
+    leftkey = false;
+  }
+  if(key.keyCode == 38){
+    downkey = false;
+  }
+  if(key.keyCode == 39){
+    rightkey = false;
+  }
+  if(key.keyCode == 40){
+    upkey = false;
+  }
+}
+
+
+function draw() {
+  canvas_2d.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawrect();
+  // randnCreate();
+  
+  if(leftkey == true){
+    x = x-speed;
+  }
+  if(downkey == true){
+    y -= speed;
+  }
+  if(rightkey == true){
+    x = x+speed;
+  }
+  if(upkey == true){
+    y += speed;
+  }
+}
+
 function createOJ(object_time){
   setInterval(function(){randnCreate()},object_time);  
 } // 5초마다 생성
 
-
-createOJ(5000);
-User();
-
+setInterval(draw,10);
+// createOJ(5000);
 timestemp();
 
  if(timer == 30){
